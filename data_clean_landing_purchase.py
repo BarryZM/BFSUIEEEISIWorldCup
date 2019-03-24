104aa3f191fc9ed7f1209bbd235c0473ace5bdb4
lenovo <1074006829@qq.com>
2019-03-24 20:25:11 +0800

    landing functions

--- data_clean_landing_purchase.py
+++ data_clean_landing_purchase.py
@@ -111,7 +111,6 @@ def primary_analysis_after_duplicate_handled():
 '''
 
 
-
 def empty_value_handle_basic_info():
     """
     empty_value handle for table 年报-企业基本信息.
@@ -168,86 +167,160 @@ def clean_announcement_of_land():
     # status_after = [1,2]
     # dcu.merge_status('temp', 'a',status_list, status_after)
 
-
+#把土地用途转换为数字
 def land_usage(file_name, column_name):
-    status_5 = ['05','051','052','053','054',u'批发零售用地',u'住宿餐饮用地',u'商务金融用地',u'其它商服用地',u'商服用地',u'05、07']
-    status_6 = [u'06', u'061', u'062', u'063', u'工业用地', u'采矿用地', u'仓储用地']
-    status_7 = [u'07', u'071', u'072',u'073',u'住宅用地', u'城镇住宅用地', u'农村宅基地', u'中低价位、中小套型普通商品住房用地',
-                     u'其他住房用地',u'其他普通商品住房用地',u'经济适用住房用地']
-    status_8 = [u'08', u'081', u'082', u'083', u'084', u'085',u'086',u'087',u'088',u'机关团体用地',u'新闻出版用地'
-                     u'科教用地', u'医卫慈善用地', u'文体娱乐用地',u'公共设施用地',u'公园与绿地',u'风景名胜设施用地']
+    status_5 = ['05','051','052','053','054', u'批发零售用地', u'住宿餐饮用地', u'商务金融用地',u'其他商服用地',u'商服用地',u'05、07']
+    status_6 = [u'06', u'061', u'062', u'063', u'工业用地', u'采矿用地', u'仓储用地', u'工矿仓储用地']
+    status_7 = [u'07', u'071', u'072',u'073',u'074',u'077',u'住宅用地', u'城镇住宅用地', u'农村宅基地', u'廉租住房用地',u'中低价位、中小套型普通商品住房用地',
+                     u'其他住房用地',u'其他普通商品住房用地',u'经济适用住房用地',u'高档住宅用地',u'公共租赁住房用地']
+    status_8 = [u'08', u'081', u'082', u'083', u'084', u'085',u'086',u'087',u'088',u'机关团体用地',u'新闻出版用地',u'新闻用地',
+                     u'科教用地', u'医卫慈善用地', u'文体娱乐用地',u'公共设施用地',u'公园与绿地',u'风景名胜设施用地',u'公共管理与公共服务用地']
     status_9 = [u'09', u'091', u'092', u'093',u'094',u'095',u'军事设施用地', u'使领馆用地', u'监教场所用地',
-                     u'宗教用地',u'殡葬用地']
+                     u'宗教用地',u'殡葬用地',u'特殊用地']
     status_10 = [u'100', u'10', u'101', u'102', u'103', u'104', u'105', u'106', u'107', u'铁路用地', u'公路用地',
-                     u'街巷用地', u'农村道路', u'机场用地', u'港口码头用地', u'管道运输用地']
+                     u'街巷用地', u'农村道路', u'机场用地', u'港口码头用地', u'管道运输用地',u'交通运输用地']
     status_11 = [u'11', u'111', u'112', u'113', u'114', u'115', u'116', u'117', u'118', u'119', u'河流水面',
                      u'湖泊水面', u'水库水面', u'坑塘水面', u'沿海滩涂', u'内陆滩涂', u'沟渠', u'水工建筑用地',
-                     u'冰川及永久积雪']
+                     u'冰川及永久积雪',u'水域及水利设施用地']
     status_12 = [u'12', u'121', u'122', u'123', u'124', u'125', u'126', u'127', u'空闲地', u'设施农用地',
-                     u'田坎', u'盐碱地', u'沼泽地', u'沙地', u'裸地']
-    status_0 = [u'0']
-    # status_list = [status_5, status_6, status_7, status_8, status_9, status_10, status_11, status_12, status_0]
-    # status_after = [5, 6, 7, 8, 9, 10, 11, 12, 0]
-    status_list = [status_5, status_6,status_7]
-    status_after = [5, 6, 7]
+                     u'田坎', u'盐碱地', u'沼泽地', u'沙地', u'裸地',u'其他土地']
+    status_0 = [u'0',u'土地面积(公顷)'] #错误的类别
+    status_list = [status_5, status_6, status_7, status_8, status_9, status_10, status_11, status_12, status_0]
+    status_after = [5, 6, 7, 8, 9, 10, 11, 12, 0]
     dcu.merge_status(file_name, column_name, status_list, status_after)
     return
 
 import data_clean_landing_purchase
-data_clean_landing_purchase.land_usage(u'购地-房地产大企业购地情况',u'土地用途')
+reload(data_clean_landing_purchase)
 
-    # info:
-    # status_normal = [u'05、07']  # 搜索满足这个条件的
-    # status_list = [status_normal]
-    # status_after = [u'商服用地和住宅用地']  # 改成这个
-    # dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
-    #
-    # status_normal = [u'05',u'051',u'052',u'053',u'054',u'批发零售用地',u'住宿餐饮用地',u'商务金融用地',u'其它商服用地']  # 搜索满足这个条件的
-    # status_list = [status_normal]
-    # status_after = [u'商服用地']  # 改成这个
-    # dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
-    #
-    # status_normal = [u'06', u'061', u'062', u'063', u'工业用地', u'采矿用地', u'仓储用地']  # 搜索满足这个条件的
-    # status_list = [status_normal]
-    # status_after = [u'工矿仓储用地']  # 改成这个
-    # dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
-    #
-    # status_normal = [u'07', u'071', u'072', u'城镇住宅用地', u'农村宅基地', u'中低价位、中小套型普通商品住房用地',
-    #                  u'其他住房用地',u'其他普通商品住房用地',u'经济适用住房用地']  # 搜索满足这个条件的
-    # status_list = [status_normal]
-    # status_after = [u'住宅用地']  # 改成这个
-    # dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
-    #
-    # status_normal = [u'08', u'081', u'082', u'083', u'084', u'085',u'086',u'087',u'088',u'机关团体用地',u'新闻出版用地'
-    #                  u'科教用地', u'医卫慈善用地', u'文体娱乐用地',u'公共设施用地',u'公园与绿地',u'风景名胜设施用地']  # 搜索满足这个条件的
-    # status_list = [status_normal]
-    # status_after = [u'公共管理与公共服务用地']  # 改成这个
-    # dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
-    #
-    # status_normal = [u'09', u'091', u'092', u'093',u'094',u'095',u'军事设施用地', u'使领馆用地', u'监教场所用地',
-    #                  u'宗教用地',u'殡葬用地']  # 搜索满足这个条件的
-    # status_list = [status_normal]
-    # status_after = [u'特殊用地']  # 改成这个
-    # dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
-    #
-    # status_normal = [u'100',u'10', u'101', u'102', u'103', u'104', u'105',u'106',u'107', u'铁路用地', u'公路用地',
-    #                  u'街巷用地', u'农村道路',u'机场用地', u'港口码头用地', u'管道运输用地']  # 搜索满足这个条件的
-    # status_list = [status_normal]
-    # status_after = [u'交通运输用地']  # 改成这个
-    # dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
-    #
-    # status_normal = [u'11', u'111', u'112', u'113', u'114', u'115', u'116', u'117', u'118', u'119',u'河流水面',
-    #                  u'湖泊水面', u'水库水面', u'坑塘水面', u'沿海滩涂', u'内陆滩涂', u'沟渠',u'水工建筑用地',
-    #                  u'冰川及永久积雪']  # 搜索满足这个条件的
-    # status_list = [status_normal]
-    # status_after = [u'水域及水利设施用地']  # 改成这个
-    # dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
-    #
-    # status_normal = [u'12', u'121', u'122', u'123', u'124', u'125', u'126', u'127', u'空闲地', u'设施农用地',
-    #                  u'田坎', u'盐碱地', u'沼泽地', u'沙地', u'裸地']  # 搜索满足这个条件的
-    # status_list = [status_normal]
-    # status_after = [u'交通运输用地']  # 改成这个
-    # dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
+
+def data_clean_landing_purchase_fdcdqygdqk():
+    """
+        Dirty value handle for table 购地-房地产大企业购地情况.xlsx.
+        First we'll drop rows that empty value is too many.
+        # ['行政区','签订日期','供地总面积（公顷）','约定动工时间', '土地用途','供应方式','最小容积率','最大容积率',
+        '成交价款（万元）','约定竣工时间']
+
+        -----------------------------
+        行政区
+        ------
+        drop
+        -----------------------------
+        签订日期
+        ------
+        no change
+        -----------------------------
+        供地总面积（公顷）
+        ------
+        turn null into 'Unknown'
+        -----------------------------
+        约定动工时间
+        ------
+        95% null, turn null into 'Unknown'
+        -----------------------------
+        土地用途
+        ------
+        turn into several integers
+        -----------------------------
+        供应方式
+        ------
+        95% null, turn null into 'Unknown',turn into several integers
+        -----------------------------
+        最小容积率
+        ------
+        98% null, turn null into 'Unknown'
+        -----------------------------
+        最大容积率
+        ------
+        98% null, turn null into 'Unknown'
+        -----------------------------
+        成交价款（万元）
+        ------
+        turn null into 'Unknown'
+        -----------------------------
+        约定竣工时间
+        ------
+        98% null, turn null into 'Unknown'
+        -----------------------------
+        """
+    dcu.drop_columns(u'购地-房地产大企业购地情况', u'行政区')
+    # 日期：需要去掉小数部分
+
+    data_clean_landing_purchase.land_usage(u'购地-房地产大企业购地情况', u'土地用途')
+
+    dcu.merge_status(u'购地-房地产大企业购地情况', u'供地总面积（公顷）', [], [])
+    dcu.merge_status(u'购地-房地产大企业购地情况', u'约定动工时间',[],[])
+    dcu.merge_status(u'购地-房地产大企业购地情况', u'最小容积率', [], [],empty_mask='-65535')
+    dcu.merge_status(u'购地-房地产大企业购地情况', u'最大容积率', [], [],empty_mask='-65535')
+    dcu.merge_status(u'购地-房地产大企业购地情况', u'成交价款（万元）', [], [],empty_mask='-65535')
+    dcu.merge_status(u'购地-房地产大企业购地情况', u'约定竣工时间', [], [])
+    dcu.merge_status(u'购地-房地产大企业购地情况', u'供应方式', [[u'划拨'],[u'协议出让'],[u'拍卖出让'],[u'招标出让'],[u'挂牌出让']], [1,2,3,4,5])
+
+    return
+
+
+def data_clean_landing_purchase_fdcddkcrqk():
+    """
+                Dirty value handle for table 购地-房地产大地块出让情况.xlsx.
+        First we'll drop rows that empty value is too many.
+        ['签订日期','供地总面积','约定动工时间', '土地用途','供应方式','容积率下限','容积率上限',
+        '成交价款（万元）','约定竣工时间']
+
+        -----------------------------
+        签订日期
+        ------
+        turn into integer
+        -----------------------------
+        供地总面积
+        ------
+        turn null into 'Unknown'
+        -----------------------------
+        约定动工时间
+        ------
+        97% null, turn null into 'Unknown'
+        -----------------------------
+        土地用途
+        ------
+        turn into several integers
+        -----------------------------
+        供应方式
+        ------
+        97% null, turn null into 'Unknown',turn into several integers
+        -----------------------------
+        容积率下限
+        ------
+        98% null, turn null into 'Unknown'
+        -----------------------------
+        容积率上限
+        ------
+        98% null, turn null into 'Unknown'
+        -----------------------------
+        成交价款（万元）
+        ------
+        86% null, turn null into 'Unknown'
+        -----------------------------
+        约定竣工时间
+        ------
+        99% null, turn null into 'Unknown'
+        -----------------------------
+        """
+    dcu.drop_columns(u'购地-房地产大地块出让情况', u'行政区')
+    # 日期：需要去掉小数部分
+
+    data_clean_landing_purchase.land_usage(u'购地-房地产大地块出让情况', u'土地用途')
+
+    dcu.merge_status(u'购地-房地产大地块出让情况', u'供地总面积', [], [])
+    dcu.merge_status(u'购地-房地产大企业购地情况', u'约定动工时间',[],[])
+    dcu.merge_status(u'购地-房地产大企业购地情况', u'最小容积率', [], [],empty_mask='-65535')
+    dcu.merge_status(u'购地-房地产大企业购地情况', u'最大容积率', [], [],empty_mask='-65535')
+    dcu.merge_status(u'购地-房地产大企业购地情况', u'成交价款（万元）', [], [],empty_mask='-65535')
+    dcu.merge_status(u'购地-房地产大企业购地情况', u'约定竣工时间', [], [])
+    dcu.merge_status(u'购地-房地产大企业购地情况', u'供应方式', [[u'划拨'],[u'协议出让'],[u'拍卖出让'],[u'招标出让'],[u'挂牌出让']], [1,2,3,4,5])
+
+    return
+
+data_clean_landing_purchase.land_usage(u'购地-房地产大地块出让情况',u'土地用途')
+
 
     # dcu.drop_columns(file_name, u'出让年限'.encode('utf-8'))
     #
