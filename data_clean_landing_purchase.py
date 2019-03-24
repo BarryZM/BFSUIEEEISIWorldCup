4da1d964edd8ae662ac2b6d872f7cfb6215ed225
lenovo <1074006829@qq.com>
2019-03-24 17:21:40 +0800

    landing comments

--- data_clean_landing_purchase.py
+++ data_clean_landing_purchase.py
@@ -143,69 +143,99 @@ def clean_announcement_of_land():
     wr2 = wr1([u'时间'.encode('utf-8')].str.split('',n = 1, expand = False )) # 使用split删除部分时间的精确时间
     fu.write_file(wr2, clean_data_temp_file_url, u'购地-地块公示_test', ext='.xlsx',
                   sheet_name='Sheet', index=False)
-
-    status_normal = [u'05、07']  # 搜索满足这个条件的
-    status_list = [status_normal]
-    status_after = [u'商服用地和住宅用地']  # 改成这个
-    dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
-
-    status_normal = [u'05',u'051',u'052',u'053',u'054',u'批发零售用地',u'住宿餐饮用地',u'商务金融用地',u'其它商服用地']  # 搜索满足这个条件的
-    status_list = [status_normal]
-    status_after = [u'商服用地']  # 改成这个
-    dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
-
-    status_normal = [u'06', u'061', u'062', u'063', u'工业用地', u'采矿用地', u'仓储用地']  # 搜索满足这个条件的
-    status_list = [status_normal]
-    status_after = [u'工矿仓储用地']  # 改成这个
-    dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
-
-    status_normal = [u'07', u'071', u'072', u'城镇住宅用地', u'农村宅基地', u'中低价位、中小套型普通商品住房用地',
-                     u'其他住房用地',u'其他普通商品住房用地',u'经济适用住房用地']  # 搜索满足这个条件的
-    status_list = [status_normal]
-    status_after = [u'住宅用地']  # 改成这个
-    dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
-
-    status_normal = [u'08', u'081', u'082', u'083', u'084', u'085',u'086',u'087',u'088',u'机关团体用地',u'新闻出版用地'
-                     u'科教用地', u'医卫慈善用地', u'文体娱乐用地',u'公共设施用地',u'公园与绿地',u'风景名胜设施用地']  # 搜索满足这个条件的
-    status_list = [status_normal]
-    status_after = [u'公共管理与公共服务用地']  # 改成这个
-    dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
-
-    status_normal = [u'09', u'091', u'092', u'093',u'094',u'095',u'军事设施用地', u'使领馆用地', u'监教场所用地',
-                     u'宗教用地',u'殡葬用地']  # 搜索满足这个条件的
-    status_list = [status_normal]
-    status_after = [u'特殊用地']  # 改成这个
-    dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
-
-    status_normal = [u'100',u'10', u'101', u'102', u'103', u'104', u'105',u'106',u'107', u'铁路用地', u'公路用地',
-                     u'街巷用地', u'农村道路',u'机场用地', u'港口码头用地', u'管道运输用地']  # 搜索满足这个条件的
-    status_list = [status_normal]
-    status_after = [u'交通运输用地']  # 改成这个
-    dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
-
-    status_normal = [u'11', u'111', u'112', u'113', u'114', u'115', u'116', u'117', u'118', u'119',u'河流水面',
-                     u'湖泊水面', u'水库水面', u'坑塘水面', u'沿海滩涂', u'内陆滩涂', u'沟渠',u'水工建筑用地',
-                     u'冰川及永久积雪']  # 搜索满足这个条件的
-    status_list = [status_normal]
-    status_after = [u'水域及水利设施用地']  # 改成这个
-    dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
-
-    status_normal = [u'12', u'121', u'122', u'123', u'124', u'125', u'126', u'127', u'空闲地', u'设施农用地',
-                     u'田坎', u'盐碱地', u'沼泽地', u'沙地', u'裸地']  # 搜索满足这个条件的
-    status_list = [status_normal]
-    status_after = [u'交通运输用地']  # 改成这个
-    dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
-
-    dcu.drop_columns(file_name, u'出让年限'.encode('utf-8'))
-
-    status_normal = [u'0']  # 搜索满足这个条件的
-    status_list = [status_normal]
-    status_after = ['Error']  # 改成这个
-    dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
-
-    dcu.drop_columns(file_name, u'出让年限'.encode('utf-8'))
-
     return
+
+    # AB =1 CD =2
+    # status_1 = [u'A', u'B']
+    # status_2 = [u'C', u'D']
+    # status_list = [status_1,status_2]
+    # status_after = [1,2]
+    # dcu.merge_status('temp', 'a',status_list, status_after)
+
+
+def land_usage(file_name, column_name):
+    status_5 = ['05','051','052','053','054',u'批发零售用地',u'住宿餐饮用地',u'商务金融用地',u'其它商服用地',u'商服用地',u'05、07']
+    status_6 = [u'06', u'061', u'062', u'063', u'工业用地', u'采矿用地', u'仓储用地']
+    status_7 = [u'07', u'071', u'072',u'073',u'住宅用地', u'城镇住宅用地', u'农村宅基地', u'中低价位、中小套型普通商品住房用地',
+                     u'其他住房用地',u'其他普通商品住房用地',u'经济适用住房用地']
+    status_8 = [u'08', u'081', u'082', u'083', u'084', u'085',u'086',u'087',u'088',u'机关团体用地',u'新闻出版用地'
+                     u'科教用地', u'医卫慈善用地', u'文体娱乐用地',u'公共设施用地',u'公园与绿地',u'风景名胜设施用地']
+    status_9 = [u'09', u'091', u'092', u'093',u'094',u'095',u'军事设施用地', u'使领馆用地', u'监教场所用地',
+                     u'宗教用地',u'殡葬用地']
+    status_10 = [u'100', u'10', u'101', u'102', u'103', u'104', u'105', u'106', u'107', u'铁路用地', u'公路用地',
+                     u'街巷用地', u'农村道路', u'机场用地', u'港口码头用地', u'管道运输用地']
+    status_11 = [u'11', u'111', u'112', u'113', u'114', u'115', u'116', u'117', u'118', u'119', u'河流水面',
+                     u'湖泊水面', u'水库水面', u'坑塘水面', u'沿海滩涂', u'内陆滩涂', u'沟渠', u'水工建筑用地',
+                     u'冰川及永久积雪']
+    status_12 = [u'12', u'121', u'122', u'123', u'124', u'125', u'126', u'127', u'空闲地', u'设施农用地',
+                     u'田坎', u'盐碱地', u'沼泽地', u'沙地', u'裸地']
+    status_0 = [u'0']
+    # status_list = [status_5, status_6, status_7, status_8, status_9, status_10, status_11, status_12, status_0]
+    # status_after = [5, 6, 7, 8, 9, 10, 11, 12, 0]
+    status_list = [status_5, status_6,status_7]
+    status_after = [5, 6, 7]
+    dcu.merge_status(file_name, column_name, status_list, status_after)
+    return
+
+import data_clean_landing_purchase
+data_clean_landing_purchase.land_usage(u'购地-房地产大企业购地情况',u'土地用途')
+
+    # info:
+    # status_normal = [u'05、07']  # 搜索满足这个条件的
+    # status_list = [status_normal]
+    # status_after = [u'商服用地和住宅用地']  # 改成这个
+    # dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
+    #
+    # status_normal = [u'05',u'051',u'052',u'053',u'054',u'批发零售用地',u'住宿餐饮用地',u'商务金融用地',u'其它商服用地']  # 搜索满足这个条件的
+    # status_list = [status_normal]
+    # status_after = [u'商服用地']  # 改成这个
+    # dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
+    #
+    # status_normal = [u'06', u'061', u'062', u'063', u'工业用地', u'采矿用地', u'仓储用地']  # 搜索满足这个条件的
+    # status_list = [status_normal]
+    # status_after = [u'工矿仓储用地']  # 改成这个
+    # dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
+    #
+    # status_normal = [u'07', u'071', u'072', u'城镇住宅用地', u'农村宅基地', u'中低价位、中小套型普通商品住房用地',
+    #                  u'其他住房用地',u'其他普通商品住房用地',u'经济适用住房用地']  # 搜索满足这个条件的
+    # status_list = [status_normal]
+    # status_after = [u'住宅用地']  # 改成这个
+    # dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
+    #
+    # status_normal = [u'08', u'081', u'082', u'083', u'084', u'085',u'086',u'087',u'088',u'机关团体用地',u'新闻出版用地'
+    #                  u'科教用地', u'医卫慈善用地', u'文体娱乐用地',u'公共设施用地',u'公园与绿地',u'风景名胜设施用地']  # 搜索满足这个条件的
+    # status_list = [status_normal]
+    # status_after = [u'公共管理与公共服务用地']  # 改成这个
+    # dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
+    #
+    # status_normal = [u'09', u'091', u'092', u'093',u'094',u'095',u'军事设施用地', u'使领馆用地', u'监教场所用地',
+    #                  u'宗教用地',u'殡葬用地']  # 搜索满足这个条件的
+    # status_list = [status_normal]
+    # status_after = [u'特殊用地']  # 改成这个
+    # dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
+    #
+    # status_normal = [u'100',u'10', u'101', u'102', u'103', u'104', u'105',u'106',u'107', u'铁路用地', u'公路用地',
+    #                  u'街巷用地', u'农村道路',u'机场用地', u'港口码头用地', u'管道运输用地']  # 搜索满足这个条件的
+    # status_list = [status_normal]
+    # status_after = [u'交通运输用地']  # 改成这个
+    # dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
+    #
+    # status_normal = [u'11', u'111', u'112', u'113', u'114', u'115', u'116', u'117', u'118', u'119',u'河流水面',
+    #                  u'湖泊水面', u'水库水面', u'坑塘水面', u'沿海滩涂', u'内陆滩涂', u'沟渠',u'水工建筑用地',
+    #                  u'冰川及永久积雪']  # 搜索满足这个条件的
+    # status_list = [status_normal]
+    # status_after = [u'水域及水利设施用地']  # 改成这个
+    # dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
+    #
+    # status_normal = [u'12', u'121', u'122', u'123', u'124', u'125', u'126', u'127', u'空闲地', u'设施农用地',
+    #                  u'田坎', u'盐碱地', u'沼泽地', u'沙地', u'裸地']  # 搜索满足这个条件的
+    # status_list = [status_normal]
+    # status_after = [u'交通运输用地']  # 改成这个
+    # dcu.merge_status(file_name, u'土地用途'.encode('utf-8'), status_list, status_after, empty_mask='Unknown')
+
+    # dcu.drop_columns(file_name, u'出让年限'.encode('utf-8'))
+    #
+    # return
 
 
 
