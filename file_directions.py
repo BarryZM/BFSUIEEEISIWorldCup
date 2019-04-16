99b9a74408ae2dd1ec903c986e507ad653d97e8a
Mike Zhu <zhuyuan2706@gmail.com>
2019-04-16 19:14:29 +0800

    添加测试集

--- file_directions.py
+++ file_directions.py
@@ -1,15 +1,29 @@
 # *- coding:utf-8 -*-
 
-origin_file_url = u'data/赛题1数据材料/赛题1数据集/赛题1数据集/'  # the original file folder url, files in it should be read only
-working_file_url = u'data/processed_data/raw_data/'  # the working file folder, a copy from the original file folder, can be changed during work
-statistic_data_file_url = u'data/processed_data/statistic_data/'  # statistic info for every table
-categorized_data_file_url = u'data/processed_data/categorized_data/'  # categorized info for every table
-cleaned_data_file_url = u'data/processed_data/cleaned_data/'  # cleaned tables
-clean_data_temp_file_url = u'data/processed_data/clean_data_temp/'  # clean temp tables
-corporation_index_file_url = u'data/processed_data/corporation_index/'  # corporation index files
-corporation_index_second_stage_file_url = u'data/processed_data/corporation_index/second_stage/'  # corporation index files in the second stage
-corporation_index_scatter_file_url = u'data/processed_data/corporation_index_scatter/'  # corporation index scatter image files
-corporation_index_heat_map_file_url = u'data/processed_data/corporation_index_heat_map/'  # corporation index heatmap image files
+# origin_file_url = u'data/赛题1数据材料/赛题1数据集/赛题1数据集/'  # the original file folder url, files in it should be read only
+# working_file_url = u'data/processed_data/raw_data/'  # the working file folder, a copy from the original file folder, can be changed during work
+# statistic_data_file_url = u'data/processed_data/statistic_data/'  # statistic info for every table
+# categorized_data_file_url = u'data/processed_data/categorized_data/'  # categorized info for every table
+# cleaned_data_file_url = u'data/processed_data/cleaned_data/'  # cleaned tables
+# clean_data_temp_file_url = u'data/processed_data/clean_data_temp/'  # clean temp tables
+# corporation_index_file_url = u'data/processed_data/corporation_index/'  # corporation index files
+# corporation_index_second_stage_file_url = u'data/processed_data/corporation_index/second_stage/'  # corporation index files in the second stage
+# corporation_index_scatter_file_url = u'data/processed_data/corporation_index_scatter/'  # corporation index scatter image files
+# corporation_index_heat_map_file_url = u'data/processed_data/corporation_index_heat_map/'  # corporation index heatmap image files
+#
+# corporate_index_false = u'企业总评分'.encode('utf-8')
+# corporate_index_true = u'企业编号'.encode('utf-8')
+
+origin_file_url = u'data/test/赛题1数据材料/赛题1测试数据集/'  # the original file folder url, files in it should be read only
+working_file_url = u'data/test/processed_data/raw_data/'  # the working file folder, a copy from the original file folder, can be changed during work
+statistic_data_file_url = u'data/test/processed_data/statistic_data/'  # statistic info for every table
+categorized_data_file_url = u'data/test/processed_data/categorized_data/'  # categorized info for every table
+cleaned_data_file_url = u'data/test/processed_data/cleaned_data/'  # cleaned tables
+clean_data_temp_file_url = u'data/test/processed_data/clean_data_temp/'  # clean temp tables
+corporation_index_file_url = u'data/test/processed_data/corporation_index/'  # corporation index files
+corporation_index_second_stage_file_url = u'data/test/processed_data/corporation_index/second_stage/'  # corporation index files in the second stage
+corporation_index_scatter_file_url = u'data/test/processed_data/corporation_index_scatter/'  # corporation index scatter image files
+corporation_index_heat_map_file_url = u'data/test/processed_data/corporation_index_heat_map/'  # corporation index heatmap image files
 
 corporate_index_false = u'企业总评分'.encode('utf-8')
 corporate_index_true = u'企业编号'.encode('utf-8')
