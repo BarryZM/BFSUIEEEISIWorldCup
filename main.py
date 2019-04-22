71a23115a5e34f6df5cc1c6dc3919a8c60e01dfa
Mike Zhu <zhuyuan2706@gmail.com>
2019-04-22 19:30:23 +0800

    mark

--- main.py
+++ main.py
@@ -2,15 +2,10 @@
 
 import sys
 
-import data_clean_annual_report as dcar
-import data_clean_soft_assets as dcsa
-import exploratory_data_analysis_annual_report as edaar
-import exploratory_data_analysis_soft_assets as edasa
-import primary_analysis as priana
+import train_model as tm
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 if __name__ == '__main__':
-    edaar.work_()
-    edasa.work_()
+    tm.train_random_forest_random_forest()
